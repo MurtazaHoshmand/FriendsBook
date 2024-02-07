cda3f42bf0ef477275b858bcbd68315e7b914f37
MurtazaHoshmand <145467980+MurtazaHoshmand@users.noreply.github.com>
2024-02-07 20:45:04 +0430

    initial files of Friends Book

--- routes/web.php
+++ routes/web.php
@@ -31,10 +31,4 @@ Route::post('/logout', [SessionController::class, 'destroy'])->middleware('auth'
 
 Route::middleware('can:admin')->group(function(){
     Route::resource('admin/posts', AdminPostController::class)->except('show');
-    // Route::get('admin/posts/create', [AdminPostController::class, 'create']);
-    // Route::post('admin/posts', [AdminPostController::class, 'store']);
-    // Route::get('admin/posts', [AdminPostController::class, 'index']);
-    // Route::get('admin/posts/{post}/edit', [AdminPostController::class, 'edit']);
-    // Route::patch('admin/posts/{post}', [AdminPostController::class, 'update']);
-    // Route::delete('admin/posts/{post}', [AdminPostController::class, 'destroy']);
 });
